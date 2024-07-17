44fa0be9e460b23a4d1f9a712f5b2d145fd95d4a
ideepakrajput <deepakkumar18112001@gmail.com>
2024-07-17 21:02:41 +0530

    mail api created

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const adminRoute = require('./routes/adminRoute.js');
 const winningRoute = require('./routes/winningRoute.js');
 const Misc = require("./models/misc.js")
 const User = require("./models/user.js")
-const PORT = process.env.PORT || 8080;
+const nodemailer = require('nodemailer');
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
@@ -22,6 +23,35 @@ app.use('/api/user', userRoute);
 app.use('/api/admin', adminRoute);
 app.use('/api/winning', winningRoute);
 
+
+app.post('/send-mail', (req, res) => {
+    const { to, subject, text } = req.body;
+
+    let transport = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.EMAIL_USERNAME,
+        to: to,
+        subject: subject,
+        text: text
+    };
+
+    transport.sendMail(mailOptions, function (err, info) {
+        if (err) {
+            return res.status(500).json({ error: err.toString() });
+        } else {
+            return res.status(200).json({ message: 'Email sent successfully!', info: info });
+        }
+    });
+});
 
 cron.schedule('0 0 * * *', async () => {
     const r = await Misc.findOne({ identity: "reset" })
